2a79602efd06a355e7a9dd9724bb225cd374625b
Lucas Pagac <lucaspagac0@gmail.com>
2021-01-05 15:36:01 -0800

    MarkovMachine functional

--- markov.js
+++ markov.js
@@ -7,22 +7,66 @@ class MarkovMachine {
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    // MORE CODE HERE
+    this.words = words;
+    this.wordsChain = this.makeChains();
   }
 
   /** set markov chains:
    *
    *  for text of "the cat in the hat", chains will be
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
-
+  
   makeChains() {
-    // MORE CODE HERE
+    const words = this.words;
+    let wordsChain = new Map();
+    for (let i = 0;i < words.length;i++) {
+      let nextWord = words[i + 1] || null;
+      if (wordsChain.has(words[i])) {
+        wordsChain.get(words[i]).push(nextWord);
+      } else {
+        wordsChain.set(words[i], [nextWord]);
+      }
+    }
+    return wordsChain;
   }
 
+  /** Get random word to initialize story */
+
+  getFirstWord() {
+    let keys = Array.from(this.wordsChain.keys());
+    let randomIndex = Math.floor(Math.random() * keys.length);
+    return keys[randomIndex];
+  }
+
+  /** return a random value from an array within a map */
+
+  getRandomWord(word) {
+    let wordOptions = this.wordsChain.get(word);
+    let randomIndex = Math.floor(Math.random() * wordOptions.length);
+    return wordOptions[randomIndex]
+  }
 
   /** return random text from chains */
 
   getText(numWords = 100) {
-    // MORE CODE HERE
+    let story = [];
+    story.push(this.getFirstWord());
+    numWords--;
+    let word;
+    while(numWords) {
+      word = story[story.length - 1];
+      let nextWord = this.getRandomWord(word);
+      if (nextWord) {
+        story = story.concat(nextWord);
+      } else {
+        return story.join(' ');
+      }
+      numWords--;
+    }
+    return story.join(' ');
   }
 }
+
+let mm = new MarkovMachine("the cat in the hat");
+console.log(mm.wordsChain.get('the'));
+console.log(mm.getText());
